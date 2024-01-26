95652977cffa22bbdaed45679c5359afc72bf9be
Liam <liamdavidjacobs@gmail.com>
2024-01-26 20:06:19 +0000

    Refactor app spawning to allow parallel testing instances on available OS ports.

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,26 +1,29 @@
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn health_check_works() {
-    // Arrange
-    spawn_app();
-
+    let address = spawn_app();
     let client = reqwest::Client::new();
-    // Act
+
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
-        .expect("Failed to execute request.");
+        .expect("Failed to execute request");
 
-    // Assert
+    // We check that the response has the 200 OK status code
     assert!(response.status().is_success());
+    // We check that the response has an empty body
     assert_eq!(Some(0), response.content_length());
 }
 
-// Launch application in the background
-fn spawn_app() -> Result<(), std::io::Error> {
-    let server = zero2prod::run().expect("Failed to bind address");
-
-    // Launch the server as a background task
+fn spawn_app() -> String {
+    // Randomize port to avoid conflicts in parallel test runs
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+    // Spawn our application as a background task
     let _ = tokio::spawn(server);
-    Ok(())
+
+    format!("http://127.0.0.1:{}", port)
 }
